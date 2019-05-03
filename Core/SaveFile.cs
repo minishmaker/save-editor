39202511191c7cd4e313189e00004699dd7c4a3d
Mike Nisbet <nisbet.mr@gmail.com>
2019-05-03 20:12:46 +0100

    SaveFile class for local edits

---
+++ Core/SaveFile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.CodeDom;
+using System.IO;
+using System.Text;
+using SaveEditor.Utilities;
+
+namespace SaveEditor.Core
+{
+    public class SaveFile
+    {
+        public uint checksum { get; private set; }
+
+        private byte[] data;
+        private Reader reader_;
+
+        public SaveFile(uint fileChecksum, byte[] fileData)
+        {
+            checksum = fileChecksum;
+            data = fileData;
+
+            Stream stream = Stream.Synchronized(new MemoryStream(data));
+            reader_ = new Reader(stream);
+
+            Console.WriteLine(GetName());
+        }
+
+        public string GetName()
+        {
+            return Encoding.Default.GetString(reader_.ReadBytes(6, 0x80));
+        }
+    }
+}
